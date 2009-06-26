25763fc3f90cf214526a8ce6624ed5a3335bf13d
David Stelter <david.stelter@gmail.com>
2009-06-26 13:08:03 -0700

    More schema plumbing, find methods added to Iphdr
    Can now successfully find an Iphdr's Event, for some reason belongs_to
    isn't working for this, though it IS working for other composite key
    relationships. Don't understand what's wrong there, but iphdr.event can
    find the right row thanks to a little SQL. Can also find Iphdr rows
    based on src_ip and dst_ip, numeric and string (dotted-octet).

--- app/models/iphdr.rb
+++ app/models/iphdr.rb
@@ -1,23 +1,73 @@
 class Iphdr < ActiveRecord::Base
   set_table_name "iphdr"
   set_primary_keys :cid, :sid
-  belongs_to :event,
-             :class_name  => "Event",
+
+# for some reason, belongs_to doesn't work here...
+# method event below is a workaround
+#  belongs_to :event,
+#             :class_name  => "Event",
+#             :foreign_key => [:cid, :sid],
+  has_one    :data,
+             :class_name  => "DataTab",
+             :foreign_key => [:cid, :sid]
+  has_one    :tcphdr,
+             :class_name  => "Tcphdr",
+             :foreign_key => [:cid, :sid]
+  has_one    :udphdr,
+             :class_name  => "Udphdr",
+             :foreign_key => [:cid, :sid] 
+  has_one    :icmphdr,
+             :class_name  => "Icmphdr",
+             :foreign_key => [:cid, :sid] 
+  has_many   :opt,
+             :class_name  => "Opt",
              :foreign_key => [:cid, :sid]
 
-  def ipsrc
-    self.ip_src
+#workaround method for broken belongs_to
+  def event 
+      @event = Event.find(:first, :conditions => "sid = #{sid} and cid = #{cid}") unless @event
+      @event
   end
 
+  def Iphdr.find_by_src_ip_string(ip_string)
+    ip_int = Iphdr.ip_string_to_int(ip_string)
+    Iphdr.find(:all, :conditions => "ip_src = #{ip_int}")
+  end
+
+  def Iphdr.find_by_dst_ip_string(ip_string)
+    ip_int = Iphdr.ip_string_to_int(ip_string)
+    Iphdr.find(:all, :conditions => "ip_dst = #{ip_int}")
+  end 
+
+  def Iphdr.find_by_src_ip_int(ip_int)
+    Iphdr.find(:all, :conditions => "ip_src = #{ip_int}")
+  end
+
+  def Iphdr.find_by_dst_ip_int(ip_int)
+    Iphdr.find(:all, :conditions => "ip_dst = #{ip_int}")
+  end 
+
   def ip_src_string
-    ip_int_to_string(self.ip_src)
+    Iphdr.ip_int_to_string(self.ip_src)
   end
 
   def ip_dst_string
-    ip_int_to_string(self.ip_dst)
+    Iphdr.ip_int_to_string(self.ip_dst)
   end
 
-  def ip_int_to_string(ip_int)
+#returns numeric IP address given a dotted-octet IP string
+  def Iphdr.ip_string_to_int(ip_string)
+    ip_int = 0
+    ip_string =~ /(\d+)\.(\d+)\.(\d+)\.(\d+)/
+    ip_int += $4.to_i
+    ip_int += $3.to_i <<  8
+    ip_int += $2.to_i << 16
+    ip_int += $1.to_i << 24
+  end
+
+
+#returns a human-readable IP address in dotted-octet notation
+  def Iphdr.ip_int_to_string(ip_int)
     ip_string = ""
     ip_string <<        "#{( ip_int >> 24)}"
     ip_string << "." << "#{((ip_int >> 16) & 0xff)}"
--- app/models/opt.rb
+++ app/models/opt.rb
@@ -1,3 +1,8 @@
 class Opt < ActiveRecord::Base
   set_table_name "opt"
+  set_primary_keys :cid, :sid, :optid
+  belongs_to :iphdr,
+             :class_name => "Iphdr",
+             :foreign_key => [:cid, :sid]
+
 end
