1fbbc2f7e22fbee22b5712525778bd88fdc625fc
David Stelter <david.stelter@gmail.com>
2009-06-28 22:03:18 -0700

    got pagination working for /signatures
    That was easy! And it will likely prove to be a little over-simplified,
    but for now it's getting the job done. So pagination is working for the
    alerts list on /signatures/detail pages, and I'm assuming it works on the
    /signatures/summary page too, but there's still only 15 sigs in the DB,
    so I can't test it yet.

--- app/controllers/signatures_controller.rb
+++ app/controllers/signatures_controller.rb
@@ -1,11 +1,11 @@
 class SignaturesController < ApplicationController
   def summary
-    @signatures = Signature.find(:all)
+    @signatures = Signature.paginate :page => params[:page], :order => 'sig_id'
   end
 
   def detail
     @signature = Signature.find(params[:id])
-    @events    = @signature.events
+    @events    = @signature.events.paginate :page => params[:page], :order => 'timestamp'
   end
 
 end
