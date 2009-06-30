ab6191ffc7237df4b9484957c56a218fa18a973a
David Stelter <david.stelter@gmail.com>
2009-06-30 11:18:31 -0700

    Slight change to pagination, not using session yet
    Made a tiny change to pagination as I'm not storing it in the session
    yet, confused myself with object lifecycle in the controller.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,11 +1,11 @@
 class EventsController < ApplicationController
   def summary
     order = 'timestamp'
-    if @summary_pager
-      @summary_pager.page = params[:page] || '1'
-    else
-      @summary_pager  = Pager.new(Event.count)
-    end
+    #if @summary_pager
+    #  @summary_pager.page = params[:page] || '1'
+    #else
+      @summary_pager  = Pager.new(Event.count, params[:page])
+    #end
     @events    = Event.find(:all, 
                             :limit => @summary_pager.per_page, 
                             :offset => @summary_pager.offset, 
