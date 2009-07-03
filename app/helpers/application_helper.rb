0917fbfac68002ddb84ad6da477f78c62f980c69
David Stelter <david.stelter@gmail.com>
2009-07-02 21:58:47 -0700

    Created IP header table helper
    The table is a little on the brute-force side, there must be a more
    elegant CSS way to force the display widths of cells, but for now it
    works.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -40,4 +40,56 @@ module ApplicationHelper
     @output
   end
 
+  def ip_header(iphdr)
+    iphdr_s = %{
+      <table border id=ipheader>
+        <tr>
+          <div display="none">
+    }
+    32.times do 
+      iphdr_s << %{<th>&nbsp;</th>}
+    end
+    iphdr_s << %{</div>
+        <tr>
+          <td colspan=4>IP Ver</td><td colspan=4>HLEN</td><td colspan=8>TOS</td><td colspan=16>PKTLEN</td>
+        </tr>
+        <tr>
+          <td colspan=4>#{iphdr.ip_ver}</td>
+          <td colspan=4>#{iphdr.ip_hlen}</td>
+          <td colspan=8>#{iphdr.ip_tos}</td>
+          <td colspan=16>#{iphdr.ip_len}</td>
+        </tr>
+        <tr>
+          <td colspan=16>IP ID</td><td colspan=3>FLAGS</td><td colspan=13>OFFSET</td>
+        </tr>
+        <tr>
+          <td colspan=16>0x#{iphdr.ip_id}</td>
+          <td colspan=3>#{iphdr.ip_flags}</td>
+          <td colspan=13>#{iphdr.ip_off}</td>
+        </tr>
+        <tr>
+          <td colspan=8>TTL</td><td colspan=8>PROTO</td><td colspan=16>CHKSUM</td>
+        </tr>
+        <tr>
+          <td colspan=8>#{iphdr.ip_ttl}</td>
+          <td colspan=8>#{iphdr.ip_proto}</td>
+          <td colspan=16>0x#{iphdr.ip_csum.to_s.hex}</td>
+        </tr>
+        <tr>
+          <td colspan=32>Src IP</td>
+        </tr>
+        <tr>
+          <td colspan=32>#{iphdr.ip_src_string}</td>
+        </tr>
+        <tr>
+          <td colspan=32>Dst IP</td>
+        </tr>
+        <tr>
+          <td colspan=32>#{iphdr.ip_dst_string}</td>
+        </tr>
+      </table> 
+    }
+    iphdr_s
+  end
+
 end
