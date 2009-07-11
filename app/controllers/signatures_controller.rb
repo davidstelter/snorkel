2f329ba2806f6a2868cdf457af4ef99e5d59ea82
David Stelter <david.stelter@gmail.com>
2009-07-11 04:12:04 -0700

    Started adding search parameters for signatures
    Need to work out some weirdness with understanding ActiveRecord and SQL
    for selecting all signatures above a given threshold of event counts.

--- app/controllers/signatures_controller.rb
+++ app/controllers/signatures_controller.rb
@@ -14,13 +14,34 @@ class SignaturesController < ApplicationController
       order = 'sig_id'
     end
 
-  
     @summary_pager = Pager.new(Signature.count, params[:page])
+    fh = { :limit  => @summary_pager.per_page,
+      :offset => @summary_pager.offset, 
+      :order  => order}
 
-    @signatures    = Signature.find(:all,
-                                    :limit  => @summary_pager.per_page,
-                                    :offset => @summary_pager.offset, 
-                                    :order  => order) 
+    cond_string = []
+    cond_hash   = {}
+  
+ #   if params[:cnt_thresh] && params[:cnt_thresh].length > 0
+ #     cond_string << "
+    if params[:sig_name] && params[:sig_name].length > 0
+      cond_string << "sig_name ILIKE :name"
+      cond_hash[:name] = "%#{params[:sig_name]}%"
+    end
+
+    if params[:pri] && params[:pri].length > 0
+      cond_string << "sig_priority = :pri"
+      cond_hash[:pri] = "#{params[:pri]}"
+    end
+
+    conditions = cond_string.join(" AND ")
+
+
+      @signatures    = Signature.find(:all, 
+                                      :limit  => @summary_pager.per_page,
+                                      :offset => @summary_pager.offset, 
+                                      :order  => order,
+                                      :conditions => [ conditions, cond_hash])
   end
 
   def detail 
