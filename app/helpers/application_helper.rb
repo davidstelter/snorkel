8ed378c73544f57543b072d114c378df49531a75
David Stelter <david.stelter@gmail.com>
2009-07-13 12:39:43 -0700

    Added support for multiple order_controls per page
    Order controls now check to make sure that any ordering params actually
    apply to the field they are ordering, so multiple controls per page will
    now work.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,25 +16,24 @@ module ApplicationHelper
     params     = request.params.dup
     ordered_up = params[:order]
     ordered_dn = params[:order_desc]
-
     params[:order]      = nil
     params[:order_desc] = nil
-    args[:field]      ||= text
+    field = args[:field] || text
 
     order_s = %{<span class="up-img">}
 
-    if ordered_up
+    if ordered_up == field
       order_s << image_tag("act-up11x11.png")
     else
-      order_s << link_to(image_tag("up11x11.png", :border => 0), params.merge(:order => args[:field]))
+      order_s << link_to(image_tag("up11x11.png", :border => 0), params.merge(:order => field))
     end
 
     order_s << %{</span>#{h(text)}<span class="dn-img">}
 
-    if ordered_dn
+    if ordered_dn == field
       order_s << image_tag("act-dn11x11.png")
     else
-      order_s << link_to(image_tag("dn11x11.png", :border => 0), params.merge(:order_desc => args[:field]))
+      order_s << link_to(image_tag("dn11x11.png", :border => 0), params.merge(:order_desc => field))
     end
 
     order_s << %{</span>}
