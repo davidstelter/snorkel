913f307d9cbfe50fd4ee50686b88bf4f1ad91492
David Stelter <david.stelter@gmail.com>
2009-08-01 18:10:52 -0700

    Made pagecount a @@ var

--- app/models/pager.rb
+++ app/models/pager.rb
@@ -3,31 +3,42 @@
 # Please see the file COPYING in the root source directory for details
 
 class Pager
-  attr_reader :page, :page_cnt, :item_cnt, :per_page
-  attr_writer :page, :page_cnt, :per_page
+  @@per_page = 25
+  attr_reader :page, :page_cnt, :item_cnt
+  attr_writer :page, :page_cnt
+
+   def Pager.per_page
+    @@per_page
+   end
+
+   def per_page
+     @@per_page
+   end
 
   def initialize(item_cnt = 1, page_str = '1', per_page = 25)
     @item_cnt = item_cnt || 1
     @page     = page_str.to_i
     @page = 1 if @page < 1
 
-    @per_page = per_page
+    @@per_page = per_page
     @page_cnt = (item_cnt / per_page)
     if (item_cnt % per_page)
       @page_cnt += 1
     end
   end
 
+
+
   def offset
-    (@page - 1) * @per_page
+    (@page - 1) * @@per_page
   end
 
   def first_item
-    (@page - 1) * @per_page + 1
+    (@page - 1) * @@per_page + 1
   end
 
   def last_item
-    last = (@page * @per_page)
+    last = (@page * @@per_page)
     if last > @item_cnt
       last = @item_cnt
     end
