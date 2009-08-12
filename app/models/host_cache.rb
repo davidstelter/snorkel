80af1613f553ce4658f2661656cb3b9f56e5835c
David Stelter <david.stelter@gmail.com>
2009-08-11 19:58:08 -0700

    Added copyright to a number of files

--- app/models/host_cache.rb
+++ app/models/host_cache.rb
@@ -1,3 +1,6 @@
+#Copyright © 2009 David Stelter
+#This software is released under the terms of the Modified BSD License
+#Please see the file COPYING in the root source directory for details
 class HostCache < ActiveRecord::Base
   @@last_updated = nil
 
