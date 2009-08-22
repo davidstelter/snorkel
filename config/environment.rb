3c5a17d288dd700c7da4c13f9b695b8c7b83cc44
stelterd <stelterd@hyperion.localdomain>
2009-08-22 04:19:28 -0700

    Updated rails to 2.3.3, removed mislav_will-paginate

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.3' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -42,7 +42,7 @@ end
 
 require 'composite_primary_keys'
 
-Rails::Initializer.run do |config|
-  config.gem 'mislav-will_paginate', :version => '~> 2.3.2',
-    :lib => 'will_paginate', :source => 'http://gems.github.com'
-end
+#Rails::Initializer.run do |config|
+#  config.gem 'mislav-will_paginate', :version => '~> 2.3.2',
+#    :lib => 'will_paginate', :source => 'http://gems.github.com'
+#end
